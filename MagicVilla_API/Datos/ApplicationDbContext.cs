dfbf54f93de395bd549fb6ee54517c86aca11d0d
JoseTapiaJara <jose.tapia@alu.ucm.cl>
2023-08-01 17:44:29 -0400

    alimentacion bd

--- MagicVilla_API/Datos/ApplicationDbContext.cs
+++ MagicVilla_API/Datos/ApplicationDbContext.cs
@@ -10,5 +10,38 @@ namespace MagicVilla_API.Datos
             
         }
         public DbSet<Villa> Villas { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Villa>().HasData(
+                new Villa()
+                {
+                    Id = 1,
+                    Nombre = "Villa Real",
+                    Detalle = "Detalle1",
+                    ImagenUrl = "",
+                    Ocupantes = 5,
+                    MetrosCuadrados = 50,
+                    Tarifa = 2000,
+                    Amenidad = "",
+                    FechaActualizacion = DateTime.Now,
+                    FechaCreacion = DateTime.Now
+                },
+                new Villa()
+                {
+                    Id = 2,
+                    Nombre = "Villa Las Rastras",
+                    Detalle = "Detalle2",
+                    ImagenUrl = "",
+                    Ocupantes = 5,
+                    MetrosCuadrados = 70,
+                    Tarifa = 3000,
+                    Amenidad = "",
+                    FechaActualizacion = DateTime.Now,
+                    FechaCreacion = DateTime.Now
+                }
+
+           );
+        }
     }
 }
---
+++ MagicVilla_API/Migrations/20230801214255_AlimentarTablaVilla.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace MagicVilla_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AlimentarTablaVilla : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Villas",
+                columns: new[] { "Id", "Amenidad", "Detalle", "FechaActualizacion", "FechaCreacion", "ImagenUrl", "MetrosCuadrados", "Nombre", "Ocupantes", "Tarifa" },
+                values: new object[,]
+                {
+                    { 1, "", "Detalle1", new DateTime(2023, 8, 1, 17, 42, 55, 681, DateTimeKind.Local).AddTicks(9671), new DateTime(2023, 8, 1, 17, 42, 55, 681, DateTimeKind.Local).AddTicks(9718), "", 50, "Villa Real", 5, 2000.0 },
+                    { 2, "", "Detalle2", new DateTime(2023, 8, 1, 17, 42, 55, 681, DateTimeKind.Local).AddTicks(9722), new DateTime(2023, 8, 1, 17, 42, 55, 681, DateTimeKind.Local).AddTicks(9724), "", 70, "Villa Las Rastras", 5, 3000.0 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}

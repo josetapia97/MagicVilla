9bf435f70220bdc53b01c8eedc1d7696db0aa363
JoseTapiaJara <jose.tapia@alu.ucm.cl>
2023-08-01 17:28:52 -0400

    integracion Entity Framework

---
+++ MagicVilla_API/Datos/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using MagicVilla_API.Modelos;
+using Microsoft.EntityFrameworkCore;
+
+namespace MagicVilla_API.Datos
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) :base(options)
+        {
+            
+        }
+        public DbSet<Villa> Villas { get; set; }
+    }
+}
